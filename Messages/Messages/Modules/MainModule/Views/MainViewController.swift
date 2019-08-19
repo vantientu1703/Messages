6fa2c653057608c047aa32f2b965d66a9e0c8299
Văn Tiến Tú <vantientu@van-tien-tu-2.net.fpt>
2019-08-19 21:19:13 +0700

    ref init project

---
+++ Messages/Messages/Modules/MainModule/Views/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Messages
+//
+//  Created by Văn Tiến Tú on 8/19/19.
+//  Copyright © 2019 Văn Tiến Tú. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
